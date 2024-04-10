7213690c7501cffde6d4cd089c4ddc8bcd09d29d
Giovana439 <116595560+Giovana439@users.noreply.github.com>
2024-04-10 10:46:07 -0300

    10/04

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo");
+const botoes = document.querySelectorAll(".botao");//variável que armazena todos os itens com a classe botão
+const textos = document.querySelectorAll(".aba-conteudo");//variável que armazena todos os itens com a classe aba-conteudo
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantidade de botoes
+
+    botoes[i].onclick = function () {//cria uma função 
+
+        for (let j = 0; j < botoes.length; j++) { // loop que executa enquanto j<quantidade de botoes
+            botoes[j].classList.remove("ativo");  //remove a palavra ativo da classe.
+            textos[j].classList.remove("ativo");  //remove a palavra ativo da classe.
         }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
+        textos[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivol = new Data("24-10-06T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo);
+
+function calculaTempo(tempoObjetivo) {
+
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24)
+    return dias + "dias" - hora + "horas" + minutos + "minutos" + segundos + "segundos";
+
 }
